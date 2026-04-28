c5cb5565cdbf063f98be5afb5d7a2578efa4a32e
sagar8080 <sagar8080@users.noreply.github.com>
2026-04-28 23:42:59 +0000

    deploy: 25abd1ce9e7d209920fff40626ffac6743dba90d

--- _next/static/Mx3FxKtvdkouEbMHYvtLp/_buildManifest.js
+++ _next/static/Mx3FxKtvdkouEbMHYvtLp/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/sagar8080.github.io//_next/:path+",
-        "destination": "/sagar8080.github.io/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
